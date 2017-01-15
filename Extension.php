e030c66fc4dd0f970f5e96a596aac802e9362ed5
dougfelton <dougfelton@dougfelton.com>
2017-01-14 23:24:14 -0500

    Add files via upload

---
+++ Extension.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Dougfelton\TwigDebugBar;
+
+class Extension extends \Twig_Extension
+{
+    public function __construct($assetPath = null)
+    {
+        $this->debugbar = new \DebugBar\StandardDebugBar();
+        $this->renderer = $this->debugbar->getJavascriptRenderer();
+        if (!is_null($assetPath)) {
+            $this->renderer->setBaseUrl($assetPath);
+        }
+    }
+
+    public function getFunctions()
+    {
+        return array(
+            new \Twig_Function('dbg_message', [$this, 'message']),
+            new \Twig_Function('dbg_render', [$this, 'render', ['is_safe' => ['html']]]),
+            new \Twig_Function('dbg_renderHead', [$this, 'renderHead', ['is_safe' => ['html']]]),
+        );
+    }
+
+    public function message($text, $label = info)
+    {
+        $this->debugbar['messages']->addMessage($text, $label);
+    }
+
+    public function render()
+    {
+        return $this->renderer->render();
+    }
+
+    public function renderHead()
+    {
+        return $this->renderer->renderHead();
+    }
+
+    public function getName()
+    {
+        return 'debugbar_extension';
+    }
+}
